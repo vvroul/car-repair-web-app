132e8ba282c2a3b91b1ceadd8ad4b7d9d7d6c437
Vasileios Vrouliotis <vvrouliotis@gmail.com>
2018-12-12 18:26:08 +0200

    [CreateNSearchBranch] - Starting search

--- src/main/java/com/example/demo/controller/mappers/UsersToUsersMapper.java
+++ src/main/java/com/example/demo/controller/mappers/UsersToUsersMapper.java
@@ -14,6 +14,12 @@ public class UsersToUsersMapper {
         userModel.setEmail(users.getEmail());
         userModel.setPassword(users.getPassword());
         userModel.setaFM(users.getaFM());
+        userModel.setFirstName(users.getFirstName());
+        userModel.setLastName(users.getLastName());
+        userModel.setAddress(users.getAddress());
+        userModel.setuType(users.getuType());
+        userModel.setvType(users.getvType());
+        userModel.setVPlate(users.getVPlate());
         return userModel;
     }
 }
